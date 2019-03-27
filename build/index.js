7c4d1535f1cfa6e8c78e2f7e42c89390a1c25a98
gastonrd7 <gaston.rd7@gmail.com>
2019-03-27 13:02:45 -0300

    subida codigo

--- build/index.js
+++ build/index.js
@@ -8,51 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const influencers_service_bus_1 = require("influencers-service-bus");
-const globalModels = require("influencers-models");
-require("dotenv/config");
-var init = false;
-var run = true;
+const webdriver1 = require("selenium-webdriver");
+var webDriver = webdriver1;
+var By = webDriver.By;
+var WebElement = webDriver.WebElement;
+var until = webDriver.until;
 (() => __awaiter(this, void 0, void 0, function* () {
-    while (run) {
-        console.log(init);
-        if (!init) {
-            yield influencers_service_bus_1.MessagingService.init();
-            init = true;
-        }
-        let name = 'backgroundService_postFeed';
-        try {
-            //Lectura del post a procesar          
-            var request = new influencers_service_bus_1.RequestPayload();
-            yield request.init(globalModels.Model.post, null, [
-                new influencers_service_bus_1.RequestWhere(influencers_service_bus_1.RequestWhereType.LESSOREQUALTHAN, "feedDt", yield (Date.now() - (10 * 60 * 1000))),
-                new influencers_service_bus_1.RequestWhere(influencers_service_bus_1.RequestWhereType.EQUAL, "feedStatus", "Idle")
-            ], {
-                feedStatus: "Fetching"
-            }, null, null, null, null, ["creationDt"], true);
-            console.log(request);
-            var response = Object.assign(yield influencers_service_bus_1.MessagingService.request(name, yield influencers_service_bus_1.formatRequest(influencers_service_bus_1.Source.STORAGE, influencers_service_bus_1.RequestEnum.DataStorage_Request.FIND_ONE_AND_UPDATE), request));
-            console.log(response);
-            if (response.entity === null)
-                run = false;
-            if (run) {
-                //Solicito actualizar Post
-                var post = new influencers_service_bus_1.ReadPostRequestContent(response.entity._id, response.entity);
-                var requestSocialMediaPost = new influencers_service_bus_1.SocialMediaRequestPayload(response.entity.platform, post);
-                console.log(requestSocialMediaPost);
-                var responseSocialMedia = Object.assign(yield influencers_service_bus_1.MessagingService.request(name, yield influencers_service_bus_1.formatRequest(influencers_service_bus_1.Source.SOCIALMEDIA, influencers_service_bus_1.RequestEnum.SocialMedia_Request.READ_POST), requestSocialMediaPost));
-                console.log(responseSocialMedia);
-                //Actualizo en la BD con la info actualizada
-                var requestUpdate = new influencers_service_bus_1.RequestPayload();
-                yield requestUpdate.init(globalModels.Model.post, null, null, { postPlatformFeed: JSON.stringify(responseSocialMedia.post), feedStatus: "Idle", feedDt: yield Date.now() }, response.entity._id, null, null, null);
-                var responseUpdate = Object.assign(yield influencers_service_bus_1.MessagingService.request(this.name, yield influencers_service_bus_1.formatRequest(influencers_service_bus_1.Source.STORAGE, influencers_service_bus_1.RequestEnum.DataStorage_Request.UPDATE), requestUpdate));
-                console.log(responseUpdate);
-            }
-        }
-        catch (err) {
-            console.log('se rompo', err);
-            //return Promise.reject("Error in company budgetAvailable. Error: " + JSON.stringify(err));
-        }
+    try {
+        var driver = yield new webDriver.Builder().forBrowser('chrome').build();
+        yield driver.get('https://www.facebook.com/');
+        yield driver.findElement(By.id("email")).sendKeys('open_vqmiush_user@tfbnw.net');
+        yield driver.findElement(By.id("pass")).sendKeys('Gmatias1234!');
+        yield driver.findElement(By.id('loginbutton')).submit();
+        console.log();
+    }
+    finally {
+        //await driver.quit();
     }
 }))();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,24 @@
-import { MessagingService, 
-    RequestPayload, 
-    RequestResponse, 
-    RequestEnum, 
-    formatRequest, 
-    Source,
-    RequestWhereType,
-    RequestWhere,
-SocialMediaRequestPayload, 
-SocialMediaRequestResponse, 
-ReadPostRequestContent} from 'influencers-service-bus';
-import * as globalModels from 'influencers-models';
-import 'dotenv/config';
-
-
-var init = false;
-var run = true;
+import * as webdriver1 from 'selenium-webdriver';
+import tags from './tags';
+var webDriver = webdriver1;
+var By = webDriver.By;
+var WebElement = webDriver.WebElement;
+var until = webDriver.until;
 
 (async () => {
-    while(run) {
-        console.log(init);
-        if (!init) {
-            await MessagingService.init();
-            init = true;
-        }
-        let name = 'backgroundService_postFeed';
+    try{
+        var driver = await  new webDriver.Builder().forBrowser('chrome').build();
 
-        try
-        {
-            //Lectura del post a procesar          
-            var request = new RequestPayload();
-            await request.init( globalModels.Model.post, null, 
-            [
-                new RequestWhere(RequestWhereType.LESSOREQUALTHAN, "feedDt",  await (Date.now() - (10 * 60 * 1000))),
-                new RequestWhere(RequestWhereType.EQUAL, "feedStatus", "Idle")
-            ],
-            {
-                feedStatus: "Fetching"
-            }, 
-            null, null, null, null, ["creationDt"], true);
-            
-            console.log(request);
-            var response : RequestResponse = Object.assign(await MessagingService.request(name, await formatRequest(Source.STORAGE, RequestEnum.DataStorage_Request.FIND_ONE_AND_UPDATE), request));
-            console.log(response);
+        await driver.get('https://www.facebook.com/');
+        await driver.findElement(By.id("email")).sendKeys('open_vqmiush_user@tfbnw.net');
+        await driver.findElement(By.id("pass")).sendKeys('Gmatias1234!');
+        await driver.findElement(By.id('loginbutton')).submit();
 
-            if (response.entity === null) run = false;
-            
-            if (run) {
-                //Solicito actualizar Post
-                var post = new ReadPostRequestContent(response.entity._id, response.entity);
+        console.log(
+        );
 
-                var requestSocialMediaPost = new SocialMediaRequestPayload(response.entity.platform, post);
-
-                console.log(requestSocialMediaPost);
-                var responseSocialMedia : SocialMediaRequestResponse = Object.assign(await MessagingService.request(name, await formatRequest(Source.SOCIALMEDIA, RequestEnum.SocialMedia_Request.READ_POST), requestSocialMediaPost));
-                console.log(responseSocialMedia);
-
-                //Actualizo en la BD con la info actualizada
-                var requestUpdate = new RequestPayload();
-                await requestUpdate.init(globalModels.Model.post, null, null, {postPlatformFeed: JSON.stringify(responseSocialMedia.post), feedStatus: "Idle", feedDt: await Date.now()}, response.entity._id, null, null, null);
-                var responseUpdate : RequestResponse = Object.assign(await MessagingService.request(this.name, await formatRequest(Source.STORAGE, RequestEnum.DataStorage_Request.UPDATE), requestUpdate));
-                console.log(responseUpdate);
-            }
-            
-        }
-        catch (err)
-        {
-            console.log('se rompo', err);
-            //return Promise.reject("Error in company budgetAvailable. Error: " + JSON.stringify(err));
-        }
+    } finally {
+        //await driver.quit();
     }
+
 })();
-
-
